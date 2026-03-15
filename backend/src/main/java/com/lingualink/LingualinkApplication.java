599dd0cc6b3bd9bc5252659a9e5b656a2a37a2c3
Kudarchik <diyarkud@gmail.com>
2026-03-15 18:19:12 +0300

    Initialize project structure and backend setup

---
+++ backend/src/main/java/com/lingualink/LingualinkApplication.java
@@ -0,0 +1,13 @@
+package com.lingualink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingualinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingualinkApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/lingualink/LingualinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingualink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingualinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
