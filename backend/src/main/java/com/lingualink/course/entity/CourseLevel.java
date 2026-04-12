18b1bf627c41267d43dc0edafab00b36c21f3286
Kudarchik <diyarkud@gmail.com>
2026-04-12 21:34:54 +0300

    Add course entity

---
+++ backend/src/main/java/com/lingualink/course/entity/CourseLevel.java
@@ -0,0 +1,5 @@
+package com.lingualink.course.entity;
+
+public enum CourseLevel {
+    A1, A2, B1, B2, C1, C2
+}
---
+++ backend/src/main/java/com/lingualink/course/entity/CourseStatus.java
@@ -0,0 +1,8 @@
+package com.lingualink.course.entity;
+
+public enum CourseStatus {
+    DRAFT,
+    PENDING,
+    PUBLISHED,
+    ARCHIVED
+}
